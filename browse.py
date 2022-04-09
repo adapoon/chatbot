a4aceed10533b01a3af1522fede81ecf562d2305
adapoon <21469393@life.hkbu.edu.hk>
2022-04-09 19:24:28 +0800

    browse by region

--- browse.py
+++ browse.py
@@ -1,4 +1,19 @@
 import mysql.connector
 
-def start(update, context):
-    update.message.reply_text("Browse")
+mydb = mysql.connector.connect(
+    host='comp7940-mysql.mysql.database.azure.com',
+    user='comp7940group2',
+    passwd='hkbuMySQL7940',
+    database='chatbot')
+
+sql = mydb.cursor()
+
+def browseByRegion():
+    cur = mydb.cursor()
+    cur.execute("SELECT region FROM chatbot.route GROUP BY region")
+    result = cur.fetchall()
+    return result
+
+print(browseByRegion())
+
+
