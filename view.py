8dba88c143b9693fcaa660d48373a605e13d82ad
tommasinino <97960464+tommasinino@users.noreply.github.com>
2022-04-11 01:19:00 +0800

    added photos to view

--- view.py
+++ view.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import logging
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
+from telegram import *
+from telegram.ext import * 
 
 def start(update, context, route_id):
     logging.info("View:")
@@ -9,16 +10,36 @@ def start(update, context, route_id):
                                   host='comp7940-mysql.mysql.database.azure.com',
                                   database='chatbot')
     cursor = cnx.cursor()
-    query = ("SELECT name, description FROM route WHERE route_id = %s")
+    query = ("SELECT name, description, region, district, image, map, length, duration, elevation FROM route WHERE route_id = %s")
     cursor.execute(query, (route_id, ))
     
-    for (name, description) in cursor:
-        if description != "":
-            msg = name + ":\n" + description
+    for (name, description, region, district, image, map, length, duration, elevation) in cursor:
+        hh = duration // 60
+        mm = duration % 60
+        if hh > 0:
+            time = str(hh) + " hr " + str(mm) + " min"
         else:
-            msg = name
+            time = str(mm) + " min"
 
-    update.callback_query.message.reply_text(msg)
+        msg  = "<b><u>" + name + "</u></b>:\n" 
+        msg += "<code>Region    :</code> " + region + "\n" 
+        msg += "<code>District  :</code> " + district + "\n" 
+        msg += "<code>Length    :</code> " + str(length) + " km\n" 
+        msg += "<code>Duration  :</code> " + time + "\n" 
+        msg += "<code>Evalation :</code> " + str(elevation) + " m\n" 
+        msg += description
+
+
+    logging.info(str(update))
+    context.bot.edit_message_reply_markup(chat_id=update.effective_chat.id, message_id = update.callback_query.message.message_id)
+
+    #update.callback_query.message.reply_html(msg)
+    #update.callback_query.message.reply_text(msg)
+    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode=ParseMode.HTML, text=msg)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo='https://comp7940images.blob.core.windows.net/images/'+image)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo='https://comp7940images.blob.core.windows.net/images/'+map)
+
+    context.bot.send_message(chat_id = chatID, text = "Select What you Like", reply_markup = NEW_Menu)
 
     cursor.close()
     cnx.close()
