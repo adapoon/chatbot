49550ef138fb96e440a276df738147efedc0b247
tommasinino <97960464+tommasinino@users.noreply.github.com>
2022-04-13 02:37:09 +0800

    added browse function

--- browse.py
+++ browse.py
@@ -1,21 +1,100 @@
 from telegram import *
 from telegram.ext import * 
 import logging, os, mysql.connector
+import view
 
 #This function show 10 route records.
 def start(update, context):
-    msg = "Browse results:\n"
+    msg = "Choose a region:\n"
+    
+    region_list = []
     cnx = mysql.connector.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASS'],
                                   host=os.environ['MYSQL_HOST'],
                                   database=os.environ['MYSQL_DTBS'])
+                                  
     cursor = cnx.cursor()
-    query = ("SELECT name, description FROM route LIMIT 10")
+    query = ("SELECT DISTINCT region FROM route ORDER BY region")
     cursor.execute(query)
+    
+    i = 1
+    for (region, ) in cursor:
+        region_list.append([InlineKeyboardButton(region, callback_data = region)])
+        i += 1
+      
+    update.message.reply_text(msg, reply_markup = InlineKeyboardMarkup(region_list))
 
-    for (name, description) in cursor:
-      msg += name+":  "+description+"\n"
-
-    update.message.reply_text(msg)
-
+    logging.info("Check point 1")
     cursor.close()
-    cnx.close() 
+    cnx.close()
+    
+    
+def show_district(update: Update, context: CallbackContext):
+    region = update.callback_query.data
+    context.bot.edit_message_text(chat_id=update.effective_chat.id, text="<b><u>"+region+"</u></b>", message_id = update.callback_query.message.message_id, parse_mode=ParseMode.HTML)
+
+    msg = "Choose a district:\n"
+    
+    logging.info("Check point 1")
+    district_list = []
+    cnx = mysql.connector.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASS'],
+                                  host=os.environ['MYSQL_HOST'],
+                                  database=os.environ['MYSQL_DTBS'])
+                                  
+    cursor = cnx.cursor()
+    query = ("SELECT DISTINCT district FROM route WHERE region = %s ORDER BY district")
+    cursor.execute(query, (region, ))
+    
+    i = 1
+    for (district, ) in cursor:
+        district_list.append([InlineKeyboardButton(district, callback_data = district)])
+        i += 1
+      
+    update.callback_query.message.reply_text(msg, reply_markup = InlineKeyboardMarkup(district_list))
+
+    logging.info("Check point 2")
+    cursor.close()
+    cnx.close()
+
+
+    return ConversationHandler.END
+    
+    
+def show_routes(update: Update, context: CallbackContext):
+    district = update.callback_query.data
+
+    logging.info("Check point 10")
+    
+    context.bot.edit_message_text(chat_id=update.effective_chat.id, text="<b><u>"+district+"</u></b>", message_id = update.callback_query.message.message_id, parse_mode=ParseMode.HTML)
+
+    msg = "Choose a route:\n"
+    
+    logging.info("Check point 11")
+    route_list = []
+    cnx = mysql.connector.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASS'],
+                                  host=os.environ['MYSQL_HOST'],
+                                  database=os.environ['MYSQL_DTBS'])
+                                  
+    cursor = cnx.cursor()
+    query = ("SELECT route_id, name FROM route WHERE district = %s ORDER BY route_id LIMIT 10")
+    cursor.execute(query, (district, ))
+    
+    i = 1
+    for (route_id, name) in cursor:
+        route_list.append([InlineKeyboardButton(name, callback_data = route_id)])
+        i += 1
+      
+    update.callback_query.message.reply_text(msg, reply_markup = InlineKeyboardMarkup(route_list))
+
+    logging.info("Check point 12")
+    cursor.close()
+    cnx.close()
+
+    return ConversationHandler.END
+    
+def show_route(update: Update, context: CallbackContext):
+    context.bot.delete_message(chat_id=update.effective_chat.id, message_id = update.callback_query.message.message_id)
+    view.show_route(update, context, update.callback_query.data)
+    logging.info("Check point 20")
+
+    return ConversationHandler.END
+    
--- chatbot.py
+++ chatbot.py
@@ -8,39 +8,69 @@ def main():
     updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
+    regions = ["Hong Kong Island", "Kowloon", "New Territories", "Outlying Islands"]
+    districts = ["Central and Western", "Eastern", "Islands", "Kowloon City", "Kwai Tsing", "Kwun Tong", "North", "Sai Kung", "Sha Tin", "Sham Shui Po", "Southern", "Tai Po", "Tsuen Wan", "Tuen Mun", "Wan Chai", "Yuen Long"]
 
     # You can set this logging module, so you will know when and why things do not work as expected
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     
 
-    # on different commands - answer in Telegram
+    # PATH: Start
     dispatcher.add_handler(CommandHandler("start", start_command))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+        
     dispatcher.add_handler(CommandHandler("search", search_command))
-    dispatcher.add_handler(CommandHandler("browse", browse_command))
     
+    # PATH: Browse
+    dispatcher.add_handler(ConversationHandler(
+                                entry_points=[CommandHandler("browse", browse_command)],
+                                states={regions[i] : [CallbackQueryHandler(browse.show_district)] for i in range(len(regions))},
+                                fallbacks=[CommandHandler('cancel', cancel)]
+                            )
+                        )
+
+    # PATH: Browse -> Region
+    dispatcher.add_handler(ConversationHandler(
+                                entry_points=[CallbackQueryHandler(browse.show_routes, pattern=districts[i]) for i in range(len(districts))],
+                                states={districts[i] : [CallbackQueryHandler(browse.show_routes)] for i in range(len(districts))},
+                                fallbacks=[CommandHandler('cancel', cancel)]
+                            )
+                        )
+                        
+    # PATH: Browse -> Region -> District
+    dispatcher.add_handler(ConversationHandler(
+                                entry_points=[CallbackQueryHandler(browse.show_route, pattern=str(i)) for i in range(1, 533)],
+                                states={i : [CallbackQueryHandler(browse.show_route)] for i in range(1,533)},
+                                fallbacks=[CommandHandler('cancel', cancel)]
+                            )
+                        )
+
+    # PATH: Vote
     dispatcher.add_handler(ConversationHandler(
                                 entry_points=[CommandHandler("vote", vote_command)],
-                                states={i : [CallbackQueryHandler(vote.show_result)] for i in range(1,518)},
+                                states={i : [CallbackQueryHandler(vote.show_result)] for i in range(1,533)},
                                 fallbacks=[CommandHandler('cancel', cancel)]
                             )
                         )
     
+    # PATH: Nearest routes
     dispatcher.add_handler(ConversationHandler(
                                 entry_points=[MessageHandler(Filters.location, location_message)],
-                                states={i : [CallbackQueryHandler(location.show_result)] for i in range(1,518)},
+                                states={i : [CallbackQueryHandler(location.show_result)] for i in range(1,533)},
                                 fallbacks=[CommandHandler('cancel', cancel)]
                             )
                         )
 
+    # PATH: Top 10
     dispatcher.add_handler(ConversationHandler(
                                 entry_points=[CommandHandler("top10", top10_command)],
-                                states={i : [CallbackQueryHandler(top10.show_result)] for i in range(1,518)},
+                                states={i : [CallbackQueryHandler(top10.show_result)] for i in range(1,533)},
                                 fallbacks=[CommandHandler('cancel', cancel)]
                             )
                         )
 
+    # PATH: Help
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    
     # To start the bot:
     updater.start_polling()
     updater.idle()
@@ -62,15 +92,17 @@ def location_message(update: Update, context: CallbackContext) -> int:
     location.start(update, context)
     return 1
  
-def search_command(update: Update, context: CallbackContext) -> None:
+def search_command(update: Update, context: CallbackContext) -> str:
     search.start(update, context)
+    return "Hong Kong Island"
     
 def vote_command(update: Update, context: CallbackContext) -> int:
     vote.start(update, context)
     return 1
     
-def browse_command(update: Update, context: CallbackContext) -> None:
+def browse_command(update: Update, context: CallbackContext) -> str:
     browse.start(update, context)
+    return "Hong Kong Island"
     
 def top10_command(update: Update, context: CallbackContext) -> int:
     top10.start(update, context)
