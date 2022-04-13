874cff88d912d6b86a649b2e0d08114502b027bd
Your Namegit config --global user.email you@example.com <benben2e09@gmail.com>
2022-04-13 12:49:08 +0800

    finish help message

--- help.py
+++ help.py
@@ -1,9 +1,10 @@
 from telegram import *
+from telegram import ParseMode
 from telegram.ext import * 
 import logging, os, mysql.connector
 import const
 
 def start(update, context):
-    update.message.reply_text('Helping you helping you.')
-    logging.info('Helping you helping you.')
+    msg="This is a bot for hikers.\n\n <b>Find Routes</b>\n /browse - choose a district to find a hiking route\n /search - type keyword(s) to search for hiking routes \n\n <b>Vote</b>\n /vote - choose a route that you like \n /top10 - view voting result : the top 10 hiking routes "
+    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
 
